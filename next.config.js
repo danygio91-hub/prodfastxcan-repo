d91f9d89dc437a6b2bb87375519a550485bd7b55
Daniel <dany.gio91@gmail.com>
2025-07-24 11:38:01 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const withPWA = require('next-pwa')({
 const nextConfig = {
   // This allows the Next.js dev server to accept requests from the
   // Firebase Studio environment.
+  allowedDevOrigins: ["*.cloudworkstations.dev", "http://localhost:3000"],
   images: {
     remotePatterns: [
       {
