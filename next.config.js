a0baf6be0eef1f6ca34f62e10055e32cfba94972
Daniel <dany.gio91@gmail.com>
2025-07-29 09:14:04 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -13,17 +13,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
-          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
