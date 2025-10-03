f01e4b18a9074216cb51f98428eaaf6fc33fafbf
Daniel <dany.gio91@gmail.com>
2025-10-03 13:10:51 +0000

    No niente, se finisco la prima fase di preparazione e per qualsiasi moti

--- src/app/admin/production-console/actions.ts
+++ src/app/admin/production-console/actions.ts
@@ -411,3 +411,4 @@ export async function resetSingleCompletedJobOrder(jobId: string, uid: string): 
     
 
 
+
--- src/app/admin/work-group-management/actions.ts
+++ src/app/admin/work-group-management/actions.ts
@@ -73,3 +73,4 @@ export async function dissolveWorkGroup(groupId: string): Promise<{ success: boo
 
 
 
+
