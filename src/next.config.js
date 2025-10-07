1b95e510a2c80d77d9f8997580f9cc1641247efa
Daniel <dany.gio91@gmail.com>
2025-10-07 12:13:46 +0000

    Se ho una situazione in cui non ho internet disponibile, l'operatore lav

--- src/next.config.js
+++ src/next.config.js
@@ -1,4 +1,3 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
@@ -10,7 +9,7 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
           { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-          // New Caching Headers
+          // New Caching Headers to prevent stale data while allowing PWA offline capabilities
           { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, proxy-revalidate' },
           { key: 'Pragma', value: 'no-cache' },
           { key: 'Expires', value: '0' },
