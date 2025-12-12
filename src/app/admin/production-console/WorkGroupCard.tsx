29b42ce87674f5a15af99d62ebfada0611e36e2b
Daniel <dany.gio91@gmail.com>
2025-12-12 08:54:08 +0000

    Failed to compile. ./src/app/admin/production-console/WorkGroupCard.tsx:

--- src/app/admin/production-console/WorkGroupCard.tsx
+++ src/app/admin/production-console/WorkGroupCard.tsx
@@ -432,6 +432,8 @@ export default function WorkGroupCard({
                               groupPhases={group.phases}
                               allOperators={allOperators}
                               onProblemClick={() => {}}
+                              onFetchAnalysis={() => {}}
+                              isAnalysisLoading={false}
                               onForceFinishClick={() => {}}
                               onRevertForceFinishClick={() => {}}
                               onToggleGuainaClick={() => {}}
