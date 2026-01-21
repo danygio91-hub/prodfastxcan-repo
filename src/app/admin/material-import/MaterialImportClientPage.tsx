f617445fb8f368c42b3614b5d75e1a5670574163
Daniel <dany.gio91@gmail.com>
2026-01-21 13:57:31 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: AlertDi

--- src/app/admin/material-import/MaterialImportClientPage.tsx
+++ src/app/admin/material-import/MaterialImportClientPage.tsx
@@ -20,6 +20,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import type { Packaging } from '@/lib/mock-data';
 
