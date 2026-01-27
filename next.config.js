84290f5baf65597e889424e6983d8a12d809cfc1
Daniel <dany.gio91@gmail.com>
2026-01-27 13:21:05 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    // This is required to allow the Next.js dev server to accept requests from
-    // the Google Cloud Workstations editor.
-    allowedDevOrigins: ["*.cloudworkstations.dev"],
-  },
   images: {
     remotePatterns: [
       {
